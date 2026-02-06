77c2e2b0cdf17035606880a4422ae5ed7a5fdab1
Mukul Rajput <mukulrajput0707@gmail.com>
2026-02-06 23:11:28 +0530

    neetcode Two Sum II - Input Array Is Sorted

---
+++ TwoSum_inputArray.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        left = 0
+        right = len(nums) - 1
+
+        # loop until both pointers meet
+        while left < right:
+            # calculate sum of elements at left and right
+            current_sum = nums[left] + nums[right]
+
+            if current_sum == target:
+                return [left+1, right+1]
+
+            
+            elif current_sum > target:
+                right -= 1
+    
+            else:
+                left += 1
+
+
+nums = [2, 3, 5, 8]
+target = 10
+
+solution = Solution()
+print(solution.twoSum(nums, target))
